27cb92480b7788ca6608fb9b6081b8e98130ce3d
vincenterc <chung.menghsun@gmail.com>
2019-12-22 00:43:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# next-typescript-starter
+
+A starter template for [Next.js][].
+
+[Next.js]: https://nextjs.org
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Index = () => <h1>Next.js Typescript Starter!</h1>
+
+export default Index
